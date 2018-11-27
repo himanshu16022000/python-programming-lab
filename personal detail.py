67f6534bc6bfa443d191be92e268a71f3e5d785c
himanshu16022000 <43983264+himanshu16022000@users.noreply.github.com>
2018-11-27 12:36:39 +0530

    Add files via upload

---
+++ personal detail.py
@@ -0,0 +1,6 @@
+d={"Name":'',"Roll no.":'',"Marks":''}
+for i in range(3):
+    d["Name"]=input("Enter name") 
+    d["Roll no."]=input("Enter Roll no.")
+    d["Marks"]=input("Enter Marks")
+    print(d)
